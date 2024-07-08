9f75c7ca0db62b7b3a356a2ffa6d878b04dcb04b
seth spring <seth.c.spring@gmail.com>
2024-07-08 11:54:03 -0400

    defined function todo_app

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
-user_prompt = "Type 'add', 'show', 'edit' or 'exit': "
+def todo_app():
+    user_prompt = "Type 'add', 'show', 'edit' or 'exit': "
 
-todo_list = []
+    todo_list = []
 
-while True:
-    user_action = input(user_prompt)
-    user_action = user_action.strip()
+    while True:
+        user_action = input(user_prompt)
+        user_action = user_action.strip()
 
-    match user_action:
-        case 'add':
-            todo = input("Enter a to-do task: ")
-            todo_list.append(todo.title())
-        #  Bitwise 'OR' operator
-        case 'show' | 'display':
-            for item in todo_list:
-                print(item)
-        case 'edit':
-            task_number = int(input("Enter the number associated with your to-do task: "))
-            task_number = task_number - 1
-            new_task = input("Edit your to-do task: ")
-            todo_list[task_number] = new_task.title()
-        case 'exit':
-            break
-        #  common variable '_' for error handling - defined on the fly
-        case _:
-            print("Please type a valid command.")
+        match user_action:
+            case 'add':
+                todo = input("Enter a to-do task: ")
+                todo_list.append(todo.title())
+            #  Bitwise 'OR' operator
+            case 'show' | 'display':
+                for item in todo_list:
+                    print(item)
+            case 'edit':
+                task_number = int(input("Enter the number associated with your to-do task: "))
+                task_number = task_number - 1
+                new_task = input("Edit your to-do task: ")
+                todo_list[task_number] = new_task.title()
+            case 'exit':
+                break
+            #  common variable '_' for error handling - defined on the fly
+            case _:
+                print("Please type a valid command.")
 
-print("Goodbye!")
+    print("Goodbye!")
+    return todo_list
+
+if __name__ == "__main__":
+    todo_app()
