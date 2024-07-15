78dcd32d79a5893c09f4e71023430920c184d9c6
seth spring <seth.c.spring@gmail.com>
2024-07-15 14:26:31 -0400

    enumerate examples

---
+++ coding exercises/enumerate_exercise2.py
@@ -0,0 +1,37 @@
+# ips = ['100.122.133.105', '100.122.133.111']
+#
+# user_choice = int(input("Enter the index of the IP you want: "))
+#
+# print(f"You chose {ips[user_choice]}")
+
+
+######################################################################
+# BUG FIXING
+######################################################################
+# 1
+####################################
+
+# menu = ["pasta", "pizza", "salad"]
+#
+# user_choice = int(input("Enter the index of the item: "))
+#
+# message = f"You chose {menu[user_choice]}."
+# print(message)
+
+###################################
+# 2
+###################################
+
+# menu = ["pasta", "pizza", "salad"]
+#
+# for i, j in enumerate(menu):
+#     print(f"{i}.{j}")
+
+###################################
+# 3
+###################################
+
+# menu = ["pasta", "pizza", "salad"]
+#
+# for i, j in enumerate(menu):
+#     print(F"{i}.{j}")
