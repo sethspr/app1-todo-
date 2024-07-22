c44f2dd4aba1561129d373812e3c0658df446f80
seth spring <seth.c.spring@gmail.com>
2024-07-22 14:33:50 -0400

    context manager code revision

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 def todo_app():
-    user_prompt = "Type 'add', 'show', 'edit', 'complete', or 'exit': "
 
     while True:
-        user_action = input(user_prompt)
+        # get user input and strip space characters from input
+        user_action = input("Type 'add', 'show', 'edit', 'complete', or 'exit': ")
         user_action = user_action.strip()
 
         match user_action:
             case 'add':
                 todo = input("Enter a to-do task: ") + "\n"
 
-                file = open('todos.txt', 'r') # (second argument 'w' for write, 'r' read)
-                todo_list = file.readlines()
-                file.close()
+                # context-manager with - as
+                with open('todos.txt', 'r') as file: # (second argument 'w' for write, 'r' read)
+                    todo_list = file.readlines()
 
                 todo_list.append(todo.title())
 
-                file = open('todos.txt', 'w')
-                file.writelines(todo_list)
-                file.close()
-            #  Bitwise 'OR' operator
+                with open('todos.txt', 'w') as file:
+                    file.writelines(todo_list)
+
             case 'show':
-                file = open('todos.txt', 'r')
-                todo_list = file.readlines()
+                with open('todos.txt', 'r') as file:
+                    todo_list = file.readlines()
+
+                # new_todos = [item.strip('\n') for item in todo_list]
+
                 for index, item in enumerate(todo_list):
-                    index = index + 1
-                    row = f"{index}-{item}"
+                    item = item.strip('\n')
+                    row = f"{index + 1}-{item}"
                     print(row)
             case 'edit':
                 task_number = int(input("Enter the number associated with your to-do task: "))
