3dc2bd2013f79fe7c8f06f0a2f696c7f9a8caabf
seth spring <seth.c.spring@gmail.com>
2024-07-23 15:34:41 -0400

    refactored code to conditional

--- main.py
+++ main.py
@@ -5,62 +5,65 @@ def todo_app():
         user_action = input("Type 'add', 'show', 'edit', 'complete', or 'exit': ")
         user_action = user_action.strip()
 
-        match user_action:
-            case 'add':
-                todo = input("Enter a to-do task: ") + "\n"
+        if 'add' in user_action or 'new' in user_action:
+            # use list slicing - this will give us the part after 'add ', l
+            todo = user_action[4:]
 
-                # context-manager with - as
-                with open('todos.txt', 'r') as file: # (second argument 'w' for write, 'r' read)
-                    todo_list = file.readlines()
+            # context-manager with - as
+            with open('todos.txt', 'r') as file: # (second argument 'w' for write, 'r' read)
+                todo_list = file.readlines()
 
-                todo_list.append(todo.title())
+            todo_list.append(todo.title())
 
-                with open('todos.txt', 'w') as file:
-                    file.writelines(todo_list)
+            with open('todos.txt', 'w') as file:
+                file.writelines(todo_list)
 
-            case 'show':
-                with open('todos.txt', 'r') as file:
-                    todo_list = file.readlines()
+        elif 'show' in user_action:
+            with open('todos.txt', 'r') as file:
+                todo_list = file.readlines()
 
-                # new_todos = [item.strip('\n') for item in todo_list]
+            # new_todos = [item.strip('\n') for item in todo_list]
 
-                for index, item in enumerate(todo_list):
-                    item = item.strip('\n')
-                    row = f"{index + 1}-{item}"
-                    print(row)
-            case 'edit':
-                task_number = int(input("Enter the number associated with your to-do task: "))
-                task_number = task_number - 1
+            for index, item in enumerate(todo_list):
+                item = item.strip('\n')
+                row = f"{index + 1}-{item}"
+                print(row)
+        elif 'edit' in user_action:
+            task_number = int(input("Enter the number associated with your to-do task: "))
+            # task_number = int(user_action[5:])
+            task_number = task_number - 1
 
-                with open('todos.txt', 'r') as file:
-                    todo_list = file.readlines()
+            with open('todos.txt', 'r') as file:
+                todo_list = file.readlines()
 
-                new_task = input("Edit your to-do task: ")
-                todo_list[task_number] = new_task.title() + '\n'
+            new_task = input("Edit your to-do task: ")
+            todo_list[task_number] = new_task.title() + '\n'
 
-                with open('todos.txt', 'w') as file:
-                    file.writelines(todo_list)
+            with open('todos.txt', 'w') as file:
+                file.writelines(todo_list)
 
-            case 'complete':
-                task_number = int(input("Enter the number associated with your completed task: "))
+        elif 'complete' in user_action:
+            # task_number = int(input("Enter the number associated with your completed task: "))
+            task_number = int(user_action[9:])
 
-                with open('todos.txt', 'r') as file:
-                    todo_list = file.readlines()
-                index = task_number - 1
-                todo_to_remove = todo_list[index].strip('\n')
-                todo_list.pop(index)
 
-                with open('todos.txt', 'w') as file:
-                    file.writelines(todo_list)
+            with open('todos.txt', 'r') as file:
+                todo_list = file.readlines()
+            index = task_number - 1
+            todo_to_remove = todo_list[index].strip('\n')
+            todo_list.pop(index)
 
-                message = f"Todo '{todo_to_remove}' was completed and removed from the list"
-                print(message)
+            with open('todos.txt', 'w') as file:
+                file.writelines(todo_list)
 
-            case 'exit':
-                break
-            #  common variable '_' for error handling - defined on the fly
-            case _:
-                print("Please type a valid command.")
+            message = f"Todo '{todo_to_remove}' was completed and removed from the list"
+            print(message)
+
+        elif 'exit' in user_action:
+            break
+        #  common variable '_' for error handling - defined on the fly
+        else:
+            print("Please type a valid command.")
 
     print("Goodbye!")
     return todo_list
