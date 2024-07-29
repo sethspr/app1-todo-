5971518fdb7af216d3b1e7cb6d1073c75b541ac6
seth spring <seth.c.spring@gmail.com>
2024-07-29 17:43:39 -0400

    comment function back in

--- bonus/bonus11_average_calc.py
+++ bonus/bonus11_average_calc.py
@@ -1,15 +1,15 @@
-# def get_average_temp():
-#     with open("files/bonus11_data.txt", "r") as file:
-#         data = file.readlines()
-#
-#     values = data[1:]
-#     values = [float(i) for i in values]
-#
-#     average_local = sum(values) / len(values)
-#     return average_local
-#
-# average = get_average_temp()
-# print(average)
+def get_average_temp():
+    with open("files/bonus11_data.txt", "r") as file:
+        data = file.readlines()
+
+    values = data[1:]
+    values = [float(i) for i in values]
+
+    average_local = sum(values) / len(values)
+    return average_local
+
+average = get_average_temp()
+print(average)
 
 
 ##### Bug Fixing Exercise #####
