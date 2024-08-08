2f3d4ba96f486112e863ad082b46c4a006f8f691
seth spring <seth.c.spring@gmail.com>
2024-08-08 14:51:02 -0400

    bug fixes and method update

--- bonus/bonus12_feet_to_meters_decoupling.py
+++ bonus/bonus12_feet_to_meters_decoupling.py
@@ -1,19 +1,7 @@
 # functions with decoupling
+from bonus.bonus12_functions import parse, convert
 
 feet_inches = input("Enter feet and inches: ")
-
-
-def parse(feet_inches):
-    parts = feet_inches.split(" ")
-    feet = float(parts[0])
-    inches = float(parts[1])
-    return {"feet": feet, "inches": inches}
-
-
-def convert(feet, inches):
-    meters = feet * 0.3048 + inches * 0.0254
-    return meters
-
 
 parsed = parse(feet_inches)
 result = convert(parsed['feet'], parsed['inches'])
